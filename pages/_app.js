45e1300d18570e18c4f937ca6c493fda658763c2
kipid <kipacti@gmail.com>
2024-11-18 15:22:10 +0900

    Feature:#4-챌린지-목록보기-중간정검

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,24 @@
 import Header from "@/components/Header";
+import { UserProvider } from "@/context/UserProvider";
+import ViewportProvider from "@/context/ViewportProvider";
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
-    <div className="container">
-      <Header />
-      <div className="subContainer">
-        <Component {...pageProps} />
-      </div>
-    </div>
-  </>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <ViewportProvider>
+          <div className="container">
+            <Header />
+            <div className="subContainer">
+              <Component {...pageProps} />
+            </div>
+          </div>
+        </ViewportProvider>
+      </UserProvider>
+    </QueryClientProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
-import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useUser } from "@/context/UserProvider";
+import { useState } from "react";
+import { getChallenges } from "@/apis/challengeService";
+import Challenge from "@/components/Challenge";
 
 export default function Home() {
+  const user = useUser();
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
   const {
     data: challenges,
     isPending,
     isError
   } = useQuery({
-    queryKey: ['challenges'],
-    queryFn: () => getChallenges(),
+    queryKey: ['challenges', query],
+    queryFn: () => getChallenges(query),
   });
+  const router = useRouter();
 
   return (
     <>
-      <Challenges />
+      <div className={styles.head}>
+        <h1>챌린지 목록</h1>
+        {true && <button className={styles.button} type="button" onClick={() => {router.push('/challenges/new')}}>신규 챌린지 신청 <Image width={16} height={16} src="/images/ic_plus.png" alt="New challenge" /></button>}
+      </div>
+      <div className={styles.subHead}>
+        <div className={styles.filter}>
+          <div className={styles.filterText}>필터</div>
+          <div className={styles.filterIcon}><Image width={16} height={16} src="/images/ic_filter.png" alt="Filter" /></div>
+          <div className={styles.filterDropdown}>
+            <div className={styles.filterDropdownItem}>최신순</div>
+            <div className={styles.filterDropdownItem}>인기순</div>
+            <div className={styles.filterDropdownItem}>참여순</div>
+          </div>
+        </div>
+        <div className={styles.search}>
+          <div className={styles.searchIcon}><Image width={16} height={16} src="/images/ic_search.png" alt="Search" /></div>
+          <input className={styles.searchInput} type="text" placeholder="챌린지 이름을 검색해보세요." value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+      </div>
+      {challenges?.map?.(challenge => <Challenge key={challenge.id} challenge={challenge} />)}
     </>
   );
 }

ca11709190b9f5b3eabfa463c69c40c27f9306a1
kipid <kipacti@gmail.com>
2024-11-20 10:02:42 +0900

    Feature:#10-챌린지-상세-보기-상단부분완료

--- pages/challenges/[challengeId].jsx
+++ pages/challenges/[challengeId].jsx
@@ -4,6 +4,7 @@ import Loading from "@/components/Loading.jsx";
 import { useViewport } from "@/context/ViewportProvider.jsx";
 import styles from "@/styles/ChallengeDetail.module.css";
 import { useQuery } from "@tanstack/react-query";
+import moment from "moment";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -23,23 +24,33 @@ function ChallengeDetail() {
 
   return (
     <main className={styles.main}>
-      <div className={styles.headContainer}>
-        <div className={styles.head}>
-          <h1>{challenge.title}</h1>
-          <div className={styles.subHead}>
-            <Field field={challenge.field} />
-            <Type type={challenge.docType} />
+      <div className={styles.challengeInfoContainer}>
+        <div className={styles.headContainer}>
+          <div className={styles.head}>
+            <h1>{challenge.title}</h1>
+            <div className={styles.subHead}>
+              <Field field={challenge.field} />
+              <Type type={challenge.docType} />
+            </div>
+          </div>
+          {/* TODO: user 가 맞을때만 수정/삭제 보이도록... */}
+          <div className={styles.kebabMenu}><Image width={1.5 * viewport.size} height={1.5 * viewport.size} src="/images/ic_kebab_menu.png" alt="Kebab menu" onClick={() => setKebabMenuShown(prev => !prev)} /></div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.description}>{challenge.description}</div>
+          <div className={styles.writerContainer}>
+            <Image width={1.5 * viewport.size} height={1.5 * viewport.size} src="/images/ic_profile.png" alt="Profile" />
+            <span>{challenge.writer}writer?</span>
           </div>
         </div>
-        {/* TODO: user 가 맞을때만 수정/삭제 보이도록... */}
-        <div className={styles.kebabMenu}><Image width={1.5 * viewport.size} height={1.5 * viewport.size} src="/images/ic_kebab_menu.png" alt="Kebab menu" onClick={() => setKebabMenuShown(prev => !prev)} /></div>
       </div>
-      <div className={styles.content}>
-        <div className={styles.description}>{challenge.description}</div>
-        <div className={styles.buttonContainer}>
-          <button className={styles.button} type="button" onClick={() => {router.push(`/challenges/${challengeId}/edit`)}}>수정</button>
-          <button className={styles.button} type="button">삭제</button>
+      <div className={styles.buttonContainer}>
+        <div className={styles.challengeDateAndParti}>
+          <div className={styles.challengeDeadLine}><Image width={1.5 * viewport.size} height={1.5 * viewport.size} src="/images/ic_alarm.svg" alt="Alarm" /> {moment(new Date(challenge.deadLine)).format("YYYY년 M월 D일 마감")}</div>
+          <div className={styles.challengeParticipants}><Image width={1.5 * viewport.size} height={1.5 * viewport.size} src="/images/ic_participants.svg" alt="Alarm" /> {challenge.participants}/{challenge.maxParticipants}</div>
         </div>
+        <button className={`${styles.button} ${styles.seeOriginal}`} type="button" onClick={() => window.open(challenge.docUrl)}>원문 보기</button>
+        <button className={styles.button} type="button" onClick={() => router.push(`/challenges/${challengeId}/edit`)}>작업 도전하기</button>
       </div>
     </main>
   );

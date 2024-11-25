3f701db1a747ed233ede75b16624bd9ecaf6d771
kipid <kipacti@gmail.com>
2024-11-25 13:47:08 +0900

    #46 ing

--- apis/workService.js
+++ apis/workService.js
@@ -9,3 +9,23 @@ export async function getWorkById(workId) {
 		return err?.response?.data || err;
 	}
 }
+
+export async function patchWorkById(workId, content) {
+	try {
+		const result = await instance.patch(`/works/${workId}`, { content });
+		return result?.data;
+	} catch (err) {
+		console.error(err);
+		return err?.response?.data || err;
+	}
+}
+
+export async function deleteWorkById(workId) {
+	try {
+		const result = await instance.delete(`/works/${workId}`);
+		return result?.data;
+	} catch (err) {
+		console.error(err);
+		return err?.response?.data || err;
+	}
+}

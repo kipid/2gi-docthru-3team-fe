8e7bf6b4927b421ac006f6fb3cf61e2035ac4b54
shinji530 <shinji53059@gmail.com>
2024-12-03 11:57:30 +0900

    Docs: 수정

--- pages/kakao/callback.js
+++ pages/kakao/callback.js
@@ -9,20 +9,16 @@ export default function KakaoCallback() {
 
   useEffect(() => {
     const handleKakaoLogin = async () => {
-      // URL에서 쿼리 파라미터 추출
       const query = new URLSearchParams(window.location.search);
       const accessToken = query.get('accessToken');
       const refreshToken = query.get('refreshToken');
       const user = query.get('user');
 
       if (accessToken && refreshToken && user) {
-        // 로컬 스토리지에 저장
         localStorage.setItem('user', JSON.stringify({ accessToken, refreshToken, user: JSON.parse(user) }));
 
-        // 사용자 정보 업데이트
         setUser(JSON.parse(user));
 
-        // 권한에 따라 페이지 이동
         const parsedUser = JSON.parse(user);
         if (parsedUser.role === 'Admin') {
           router.push('/admin/manage');
